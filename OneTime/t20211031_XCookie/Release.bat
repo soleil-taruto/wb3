f5fc3dc1f3cce7352c3280e94db0bfed713d502e
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-10-31 20:42:13 +0900

    Voyager 1 is 23108797173.335 kilometers away from the Sun.

--- OneTime/t20211031_XCookie/Release.bat
+++ OneTime/t20211031_XCookie/Release.bat
@@ -4,4 +4,4 @@ COPY /B Client\Claes20200001\bin\Release\Claes20200001.exe out\Client.exe
 COPY /B Server\Claes20200001\bin\Release\Claes20200001.exe out\Server.exe
 C:\Factory\Tools\xcp.exe doc out
 
-C:\Factory\SubTools\zip.exe /O out HTTCmd
+C:\Factory\SubTools\zip.exe /O out XCookie
