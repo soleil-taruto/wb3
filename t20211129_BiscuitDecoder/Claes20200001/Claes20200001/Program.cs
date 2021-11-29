d4d61ca0159648e7be4f4e7aa4e0ad8001993163
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-11-29 21:34:15 +0900

    Voyager 1 is 23151186599.612 kilometers away from the Sun.

---
+++ t20211129_BiscuitDecoder/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4();
+			}
+			Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			// -- choose one --
+
+			Main4();
+			//new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+
+			//Common.Pause();
+		}
+
+		private void Main4()
+		{
+			// main
+		}
+
+		// 以下様式統一のためのサンプル -- ★要削除
+
+#if false // コマンド引数有り -- ★要削除
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4(ar);
+			}
+			Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			// -- choose one --
+
+			Main4(new ArgsReader(new string[] { }));
+			//new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+
+			//Common.Pause();
+		}
+
+		private void Main4(ArgsReader ar)
+		{
+			try
+			{
+				Main5(ar);
+			}
+			catch (Exception e)
+			{
+				ProcMain.WriteLog(e);
+			}
+		}
+
+		private void Main5(ArgsReader ar)
+		{
+			// TODO
+		}
+#endif
+
+#if false // テスト用プログラム -- ★要削除
+		private void Main3()
+		{
+			Main4();
+			//Common.Pause();
+		}
+
+		private void Main4()
+		{
+			// -- choose one --
+
+			Main5();
+			//new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+		}
+
+		private void Main5()
+		{
+			// main
+		}
+#endif
+	}
+}
