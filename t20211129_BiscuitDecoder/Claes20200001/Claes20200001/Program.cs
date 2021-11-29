bd71d42edd5a1e31f2d287fa15e9ad3fdab74872
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-11-30 01:03:46 +0900

    Voyager 1 is 23151399074.462 kilometers away from the Sun.

--- t20211129_BiscuitDecoder/Claes20200001/Claes20200001/Program.cs
+++ t20211129_BiscuitDecoder/Claes20200001/Claes20200001/Program.cs
@@ -89,7 +89,6 @@ namespace Charlotte
 							{
 								string sData = pair[1];
 								sData = Common.ZEnc(sData);
-								sData = PutBase64Padding(sData);
 								byte[] data = SCommon.Base64.I.Decode(sData);
 								buff.Add(data);
 							}
@@ -98,11 +97,6 @@ namespace Charlotte
 				}
 			}
 			File.WriteAllBytes(Common.NextOutputPath() + ".txt", SCommon.Join(buff));
-		}
-
-		private static string PutBase64Padding(string str)
-		{
-			return str + new string[] { "", "===", "==", "=" }[str.Length % 4];
 		}
 	}
 }
